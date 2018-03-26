303d9b00cdb1b32243d406e9e1e5624286ceb11b
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-03-25 21:00:50 -0400

    Add c01 exercises 5-8

---
+++ c01/smile.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void sm(void) { printf("Smile!"); }
+
+int main(void)
+{
+    sm(); sm(); sm(); printf("\n");
+    sm(); sm(); printf("\n");
+    sm(); printf("\n");
+
+    return 0;
+}
---
+++ c01/toes.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{   
+    int toes = 10;
+
+    printf("Toes:\t%4d\nToes*2:\t%4d\nToes^2:\t%4d\n", toes, toes * 2, toes * toes);
+
+    return 0;
+}

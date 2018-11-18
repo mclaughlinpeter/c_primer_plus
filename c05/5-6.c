14a7e9bbcd3ae886d493d3f286d3c2d8086f88d4
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-11-18 01:27:54 -0500

    Add Ex 5-6

---
+++ c05/5-6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int input = 0;
+    printf("Enter number of days: ");
+    while (scanf("%d", &input) != 1 || input <= 0)
+    {
+        while(getchar() != '\n');
+        printf("Enter number of days: ");
+    }
+
+    int sum = 0;
+    int count = 1;
+    while(count <= input)
+    {
+        sum += count * count;
+        ++count;
+    }
+    printf("sum = $%d\n", sum);
+
+    return 0;
+}

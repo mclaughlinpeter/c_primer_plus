c08287fbea80e3faf82b18451793820d0c02f250
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-12-29 11:34:57 -0500

    Add Ex 11-11

--- c11/11-11.c
+++ c11/11-11.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #define LINES 10
 #define LIM 30
@@ -8,6 +10,10 @@ char get_menu_choice(void);
 char * s_gets(char * st, int n);
 int read_strings(int n_lines, int line_length, char array[][line_length], char ** pstr);
 void print_strings(char ** pstr, int n_lines);
+void sort_ascii_seq(char ** pstr, int n_lines);
+void sort_length_string(char ** pstr, int n_lines);
+void sort_length_word(char ** pstr, int n_lines);
+size_t strlen_first_word(const char * str);
 
 int main(void)
 {
@@ -18,26 +24,28 @@ int main(void)
 	int ct = read_strings(LINES, LIM, array, pstr);
 	printf("Strings read: %d\n", ct);
 	print_strings(pstr, ct);
-	/*
+	
 	char menu_choice;
 	while ((menu_choice = get_menu_choice()) != 'q')
 	{
 		switch (menu_choice)
 		{
 			case '1':
-				// print strings
+				for (int i = 0; i < ct; ++i)
+					pstr[i] = array[i];
+				print_strings(pstr, ct);
 				break;
 			case '2':
 				sort_ascii_seq(pstr, ct);
-				// print strings
+				print_strings(pstr, ct);
 				break;
 			case '3':
 				sort_length_string(pstr, ct);
-				// print strings
+				print_strings(pstr, ct);
 				break;
 			case '4':
 				sort_length_word(pstr, ct);
-				// print strings
+				print_strings(pstr, ct);
 				break;
 			case 'q':
 				exit(0);
@@ -46,14 +54,26 @@ int main(void)
 
 		}
 	}
-	*/
+	
 	return 0;
 }
 
 char get_menu_choice(void)
 {
-
-
+	puts("Enter menu choice:");
+	puts("  1) print strings           2) print by ascii sort");
+	puts("  3) print by string length  4) print by 1st word length");
+	puts("  q) quit");
+	char input;
+	while ((input = getchar()) && ((input < '1' || input > '4') && tolower(input) != 'q'))
+	{
+		if (input != '\n')
+			while (getchar() != '\n');
+		printf("Re-enter menu choice: ");
+	}
+	while (getchar() != '\n');
+	
+	return tolower(input);
 }
 
 char * s_gets(char * st, int n)
@@ -86,4 +106,50 @@ void print_strings(char ** pstr, int n_lines)
 	for (int i = 0; i < n_lines; ++i)
 		puts(pstr[i]);
 	return;
+}
+
+void sort_ascii_seq(char ** pstr, int n_lines)
+{
+	for (int i = 0; i < n_lines - 1; ++i)
+		for (int j = i + 1; j < n_lines; ++j)
+			if (strcmp(pstr[i], pstr[j]) > 0)
+			{
+				char * temp = pstr[i];
+				pstr[i] = pstr[j];
+				pstr[j] = temp;
+			}
+}
+
+void sort_length_string(char ** pstr, int n_lines)
+{
+	for (int i = 0; i < n_lines - 1; ++i)
+		for (int j = i + 1; j < n_lines; ++j)
+			if (strlen(pstr[i]) > strlen(pstr[j]))
+			{
+				char * temp = pstr[i];
+				pstr[i] = pstr[j];
+				pstr[j] = temp;
+			}
+}
+
+void sort_length_word(char ** pstr, int n_lines)
+{
+	for (int i = 0; i < n_lines - 1; ++i)
+		for (int j = i + 1; j < n_lines; ++j)
+			if (strlen_first_word(pstr[i]) > strlen_first_word(pstr[j]))
+			{
+				char * temp = pstr[i];
+				pstr[i] = pstr[j];
+				pstr[j] = temp;
+			}
+}
+
+size_t strlen_first_word(const char * str)
+{
+	while (*str && isspace(*str))
+		++str;
+	const char * ps = str;
+	while (*ps && !isspace(*ps))
+		++ps;
+	return ps - str;
 }
